00a88f023fe385d6ffc9edcc7059e975e0cb9848
Adhithya200503 <adhithya.s.200503@gmail.com>
2025-06-15 00:52:11 +0530

    added controllers and routes for zap store version3

--- controller/ZapStoreControllers.js
+++ controller/ZapStoreControllers.js
@@ -187,8 +187,8 @@ export const createZapStore = async (req, res) => {
 
     const docRef = await db.collection("zapStores").add(storeData);
     await docRef.update({ storeId: docRef.id });
-
-    res.status(201).json({ message: "Store created successfully", storeId: docRef.id , storeData });
+    storeData = {...storeData,storeId:docRef.id};
+    res.status(201).json({ message: "Store created successfully", storeData });
 
   } catch (error) {
     console.error("Error creating store:", error);
