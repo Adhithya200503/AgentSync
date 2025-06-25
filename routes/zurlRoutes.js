6daddc3d5f261799e0057d762abb280d8683f228
Adhithya200503 <adhithya.s.200503@gmail.com>
2025-06-25 09:21:02 +0530

    added controller to get all the zaplinks created by the user and routes

--- routes/zurlRoutes.js
+++ routes/zurlRoutes.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { createShortUrl} from "../controller/zurlControllers.js";
+import { getAllUserZapLinks } from "../controller/zapLinkControllers.js";
 const router = express.Router();
 router.post("/create-short-url",createShortUrl);
+router.get("/get-user-zaplinks",getAllUserZapLinks);
 export default router;
