5620090cbf6327ca27fde6926518e903e413ab33
Adhithya200503 <adhithya.s.200503@gmail.com>
2025-07-18 20:23:18 +0530

    modified protected url logic 2

--- controller/zurlControllers.js
+++ controller/zurlControllers.js
@@ -251,7 +251,7 @@ export const redirectShortUrl = async (req, res) => {
   }
 
   if (data.protected) {
-    const protectedRedirectBase = `https://agentsync-5ab53.web.app/Zurl/unlock/${shortId}`;
+    const protectedRedirectBase = `https://agentsync-5ab53.web.app/Zurl/unlock`;
     return res.redirect(`${protectedRedirectBase}/${shortId}`);
   }
 
